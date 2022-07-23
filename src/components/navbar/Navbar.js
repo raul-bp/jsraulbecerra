f89eab20098f3260e8e79a7456e12ccccd910d39
RaulBp <raul_bp97@hotmail.com>
2022-07-23 16:32:50 -0500

    Subiendo navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,18 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className='Navbar'>
+            <div>
+                <h1>My navbar</h1>
+            </div>
+            <div>
+                <button>Producto</button>
+                <button>Vasos</button>
+                <button>Tiritas</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
